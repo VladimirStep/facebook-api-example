5ff7db7dd684112a855a0f067b5e164004bc7bc5
Vladimir PC <vladimir_invest@inbox.ru>
2017-09-24 21:50:27 +0300

    Set correct data to get from Facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,18 @@ class User
 
   def self.get_info(token)
     user_api = Koala::Facebook::API.new(token)
-    profile = user_api.get_object('me')
-    feeds = user_api.get_connection('me', 'feed')
-    # post_id = feeds.first['id']
+    result = user_api.batch do |batch_api|
+      batch_api.get_object('me', { fields: ['id', 'name', 'picture'] })
+      batch_api.get_connection('me', 'posts', { limit: 5, fields: ['id', 'message', 'created_time', 'picture', 'likes', 'comments', 'shares'] })
+    end
+
+    # profile = user_api.get_object('me')
+    # posts = user_api.get_connection('me', 'posts', {limit: 5})
+    # post_id = posts.first['id']
     # post_likes = user_api.get_connections(post_id, 'likes')
-    [profile, feeds]#, post_id, post_likes]
+    # post_comments = user_api.get_connections(post_id, 'comments')
+    # [profile, feeds]#, post_id, post_likes]
+    result
   rescue Koala::Facebook::AuthenticationError => err
     session.delete(:user_token)
     nil
