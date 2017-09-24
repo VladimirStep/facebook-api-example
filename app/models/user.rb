8bea4580b38835dd38be43ff283f47cd157920dd
Vladimir PC <vladimir_invest@inbox.ru>
2017-09-24 20:20:47 +0300

    Get user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User
 
   def self.get_info(token)
     user_api = Koala::Facebook::API.new(token)
+    profile = user_api.get_object('me')
     feeds = user_api.get_connection('me', 'feed')
     # post_id = feeds.first['id']
     # post_likes = user_api.get_connections(post_id, 'likes')
-    [feeds]#, post_id, post_likes]
+    [profile, feeds]#, post_id, post_likes]
   rescue Koala::Facebook::AuthenticationError => err
     session.delete(:user_token)
     nil
