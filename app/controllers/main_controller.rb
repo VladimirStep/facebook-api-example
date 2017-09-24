c9fb6d890196bd93f084f5a285256ead8b9f6397
Vladimir PC <vladimir_invest@inbox.ru>
2017-09-24 23:23:38 +0300

    Style header

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    @user = User.facebook_api(session[:user_token]) if session[:user_token]
+    @user, @posts = User.facebook_api(session[:user_token]) if session[:user_token]
   end
 
   def auth_fail
