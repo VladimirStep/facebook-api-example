c62a90225a38467fc7de4a02c4a27e496192c7d2
Vladimir PC <vladimir_invest@inbox.ru>
2017-09-24 20:10:37 +0300

    Experiment with feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User
   def self.get_info(token)
     user_api = Koala::Facebook::API.new(token)
     feeds = user_api.get_connection('me', 'feed')
-    post_id = feeds.first['id']
-    post_likes = user_api.get_connections(post_id, 'likes')
-    [feeds, post_id, post_likes]
+    # post_id = feeds.first['id']
+    # post_likes = user_api.get_connections(post_id, 'likes')
+    [feeds]#, post_id, post_likes]
   rescue Koala::Facebook::AuthenticationError => err
     session.delete(:user_token)
     nil
